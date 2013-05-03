3930ad7bb5ba24cab4cd67d909fc433253d2a3a5
Lars Feyerabend <post@lars-feyerabend.de>
2013-05-03 15:47:23 +0200

    overhaul

--- index.php
+++ index.php
@@ -3,56 +3,36 @@
 <head>
 	<title>Lars Feyerabend</title>
 	<meta name="description" content="web developer, hobby photographer, geek.">
-	<meta name="GOOGLEBOT" content="NOSNIPPET" /> 
+	<meta name="GOOGLEBOT" content="NOSNIPPET" />
 	<meta name="author" content="Lars Feyerabend">
-	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="viewport" content="minimum-scale=1.0, width=device-width, maximum-scale=0.6667, user-scalable=no" />
-	<link rel="shortcut icon" href="i/av.png"/>
-	<link rel="apple-touch-icon" href="i/apple-touch-icon.png"/>
-	<style type="text/css" media="screen">
-		<?php $colors = array('a71c0d','21a700','2995dd'); // r,g,b ?>
-		h1{color: #<?php echo $c=$colors[array_rand($colors)]; ?>;}
-	</style>
-	<link rel="stylesheet" href="screen.min.css?v=3" media="screen" />
+	<!--[if !IE]> -->
+	<link rel="stylesheet" href="screen.min.css?v=4" media="screen" />
+	<!-- <![endif]-->
 	<script>var _gaq=[['_setAccount','UA-9868987-2'],['_trackPageview']];(function(d, t) {var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s);})(document,'script');</script>
-	<script src="http://use.typekit.com/kyj7vjn.js"></script>
-	<script>try{Typekit.load();}catch(e){}</script>
 </head>
 <body>
-	<div id="c">
-		<h1><span></span>Lars Feyerabend</h1>
-		<h2>+49 151 22631969</h2>
-		
-		<h3>Contact</h3>
-		<ul class="info">
-			<li><span>address</span><p>Krakaustraße 27a<br />52064 Aachen<br />Germany</p></li>
-			<li><span>phone</span><p><a href="tel:+4915122631969">+49 151 22631969</a></p></li>
-			<li><span>e-mail</span><p><a href="mailto:post@lars-feyerabend.de">post@lars-feyerabend.de</a></p></li>
-		</ul>
-		
-		<div id="menu">
-		<ul class="menu" id="a">
-			<li><a href="feyerabend_lars.vcf" id="vc" title="Download my vCard">Download vCard<span></span></a></li>
-			<li><a href="skype:lars.feyerabend?call" id="sk" title="Call me on Skype">Call me on Skype<span></span></a></li>
-			<li><a href="mailto:post@lars-feyerabend.de" id="em" title="Send me an e-mail">Send me an e-mail<span></span></a></li>
-		</ul>
-		<h3>Social Networks</h3>
-		<ul class="menu" id="b">
-			<li><a href="http://facebook.com/lars.feyerabend" id="fb" title="My Facebook profile">Facebook<span></span></a></li>
-			<li><a href="http://twitter.com/pulsar84" id="tw" title="Follow me on Twitter">Twitter<span></span></a></li>
-			<li class="m"><a href="http://blog.lars-feyerabend.de/" id="tu" title="My tumblelog">Tumblr<span></span></a></li>
-			<li><a href="http://flic.kr/lars-feyerabend" id="fl" title="My photography on Flickr">Flickr<span></span></a></li>
-			<li><a href="http://pinboard.in/u:pulsar/" id="dl" title="My bookmarks on Pinboard.in">Pinboard.in<span></span></a></li>
-			<li><a href="http://last.fm/user/pulsar84" id="as" title="My musical taste on last.fm">Last.fm<span></span></a></li>
-		</ul>
-		</div>
-	</div>
-	<!-- Social Media Icons by PAUL ROBERT LLOYD: http://paulrobertlloyd.com/2009/06/social_media_icons -->
-	<div id="f">
-		Based on <a href="http://iwebkit.net">iWebKit</a> &bull; Icons by <a href="http://paulrobertlloyd.com/2009/06/social_media_icons">Paul Robert Lloyd</a>
-	</div>
-	
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-	<script src="tip.js"></script>
+	<?php require_once 'MobileDetect.php'; $detect = new MobileDetect(); $m = $detect->isMobile(); ?>
+	<h1>Lars Feyerabend</h1>
+	<ul>
+		<?php if (!$m): ?>
+		<li><a href="feyerabend_lars.vcf" data-icon="V" download><span>Download vCard</span></a></li>
+		<?php endif; ?>
+		<li><a href="https://facebook.com/lars.feyerabend" rel="me" data-icon="F"><span>Facebook</span></a></li>
+		<li><a href="https://twitter.com/pulsar84" rel="me" data-icon="T"><span>Twitter</span></a></li>
+		<li><a href="https://alpha.app.net/pulsar" rel="me" data-icon="A"><span>App.net</span></a></li>
+	</ul>
+	<?php if ($m): ?>
+	<dl>
+	  <dt>phone</dt>
+	  <dd><a href="tel:+4915122631969">+49 151 2263 1969</a></dd>
+
+	  <dt>email</dt>
+	  <dd><a href="mailto:post@lars-feyerabend.de">post@lars-feyerabend.de</a></dd>
+
+	  <dt>skype</dt>
+	  <dd><a href="skype:lars.feyerabend?call">lars.feyerabend</a></dd>
+	</dl>
+	<?php endif; ?>
 </body>
 </html>
